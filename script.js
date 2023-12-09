85c2d06c6d577e9409d9fbc5826b568e96bab802
Valeria Chavez <150540121+Valeriach40718@users.noreply.github.com>
2023-12-09 02:23:05 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+/*
+Javascript es un lenguaje de programación de tipado debil que corre en el navegador
+*/
+/* Variable */
+
+/* elegir el tipo de cajita
+
+    let: son para cajita que les podemos cambiar la estructura
+
+    const: cajitas que no les puedes cambiar la estructura
+
+        innamovible: si son datos sencillos, numeros, palabras, booleanos
+        pero para las listas y objetos lo importante es que siguan siendo o listas o objetos pero si les puedes poner 
+        mas objetos adentro a las listas o sacarle objetos y a los objetos ponerles o quitarles propiedades.
+
+*/
+
+const nombre = "Valeria Chavez"
